561b9e2e3316e6d442e14b6f7194557abe8c2483
Henrik Metternich <henrik-metternich@web.de>
2023-10-27 13:42:51 +0200

    add bibkey to referenceView

--- popup/popup_interaction.js
+++ popup/popup_interaction.js
@@ -111,12 +111,6 @@ const BINPopup = ( function () {
 			//parse bib data
 			let contentString = "";
 			contentString = parser.parse(abbrevs);
-			const textArea = document.getElementById("referenceBibtex");
-			textArea.value = contentString;
-			textArea.scrollTop = 0;
-			console.log(contentString)
-
-
 
 			addProperties(BINParser.toJSON(contentString)[0])
 
@@ -263,14 +257,23 @@ const BINPopup = ( function () {
 		for (const [key, value] of Object.entries(entryTags)) {
 			addProperty(referenceProperties, key, value, "text")
 		}
+		referenceAddProperty = document.getElementById("referenceAddProperty")
+		referenceAddProperty.addEventListener("click", function() {
+			addProperty(referenceProperties, "", "", "text")
+		});
+		referenceTitle = document.getElementById("referenceTitle")
+		referenceTitle.innerHTML = "@" + object["citationKey"]
 	}
 
 	function addProperty(root, key, value, type) {
-		property = 
+		svgPath = "../icons/lucide/alert-triangle.svg"
+		property=
 		`
 		<div class="metadata-property"  tabindex="0" data-property-key="date" data-property-type="date">
 			<div class="metadata-property-key">
-				<span class="metadata-property-icon" aria-disabled="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-text"><path d="M17 6.1H3"></path><path d="M21 12.1H3"></path><path d="M15.1 18H3"></path></svg></span><input id="metadata-key-${key}" class="metadata-property-key-input" type="text" value="${key}">
+				<span class="metadata-property-icon" aria-disabled="false">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-text"><path d="M17 6.1H3"></path><path d="M21 12.1H3"></path><path d="M15.1 18H3"></path></svg>
+					</span><input id="metadata-key-${key}" class="metadata-property-key-input" type="text" value="${key}">
 			</div>
 			<div class="metadata-property-value">
 				<div class="metadata-input-longtext mod-truncate" placeholder="Empty" contenteditable="true" tabindex="0">${value}</div><div class="metadata-link" style="display: none;"><div class="metadata-link-inner">${value}</div><div class="metadata-link-flair"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-pencil"><line x1="18" y1="2" x2="22" y2="6"></line><path d="M7.5 20.5 19 9l-4-4L3.5 16.5 2 22z"></path></svg></div></div></div><div class="clickable-icon metadata-property-warning-icon" aria-label="Type mismatch, expected Text" style="display: none;"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-alert-triangle"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
