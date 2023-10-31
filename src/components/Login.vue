775180398a2c7006aa2526c337d1833b86aa3bec
noahfarr <login@noahfarr.com>
2023-10-31 18:06:09 +0100

    Initiaized Vue

---
+++ src/components/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="login">
+    <input id="tokenInputField" type="password" placeholder="Enter token here">
+    <button id="tokenButton" class="btn-cta" type="submit">Submit</button>
+  </div>
+</template>
+
+<script>
+</script>
