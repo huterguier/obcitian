789b9f81a8e388e486b869f20bad01ddd424ee3a
noahfarr <login@noahfarr.com>
2023-11-01 18:06:25 +0100

    fixed css

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="login">
-    <div class="input-group">
+  <div class="obsidian theme-dark">
       <input id="tokenInputField" v-model="token" type="password" placeholder="Enter token here" class="input-field">
-    </div>
-    <div class="button-group">
       <button id="tokenButton" @click="setApiKey" class="mod-cta" type="submit">Submit</button>
-    </div>
   </div>
 </template>
 
@@ -22,37 +18,5 @@ const setApiKey = () => {
 </script>
 
 <style scoped>
-.login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  width: 300px;
-  margin: auto;
-}
-
-.input-group,
-.button-group {
-  margin: 10px 0;
-}
-
-.input-field {
-  padding: 10px;
-  width: 100%;
-  box-sizing: border-box;
-}
-
-button.mod-cta {
-  background-color: rgb(138, 92, 245);
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
-
-button.mod-cta:hover {
-  background-color: rgb(118, 72, 225);
-}
+@import "../assets/obsidian.css";
 </style>
