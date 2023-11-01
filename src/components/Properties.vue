23f3d1a38c6db8370760a8099f95bde35c7d1f5b
noahfarr <login@noahfarr.com>
2023-11-01 19:19:55 +0100

    added dynamic property loading

--- src/components/Properties.vue
+++ src/components/Properties.vue
@@ -1,12 +1,23 @@
 <script>
 import Property from '../components/Property.vue';
 
-
 export default {
-    components: {
+  components: {
     Property,
-},
-}
+  },
+  data() {
+    return {
+      properties: [{key: "InitialKey", value: "InitialValue"}], // Start with one Property component
+    };
+  },
+  methods: {
+    handleAddProperty() {
+      // Add a new object to the properties array
+      // This will cause a new Property component to be rendered
+      this.properties.push({ key: "NewKey", value: "NewValue"});
+    },
+  },
+};
 </script>
 
 <template>
@@ -18,9 +29,17 @@ export default {
         </div>
         <div class="metadata-content">
             <div id="referenceProperties" class="metadata-properties">
-                <Property />
+                <Property v-for="(property, index) in properties" :key="index" :propertyKey="property.key" :propertyValue="property.value" />
             </div>
-            <div id="referenceAddProperty" class="metadata-add-button text-icon-button" tabindex="0"><span class="text-button-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></span><span class="text-button-label">Add property</span></div>
+            <div @click="handleAddProperty" id="referenceAddProperty" class="metadata-add-button text-icon-button" tabindex="0">
+                <span class="text-button-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-plus">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </span>
+                <span class="text-button-label">Add property</span>
+            </div>
         </div>
     </div>
 </template>
--- src/components/Property.vue
+++ src/components/Property.vue
@@ -1,44 +1,38 @@
 <template>
-    <div class="metadata-property" tabindex="0" data-property-key="date" data-property-type="date">
-        <div class="metadata-property-key">
-            <span class="metadata-property-icon" aria-disabled="false">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-text">
-                    <path d="M17 6.1H3"></path>
-                    <path d="M21 12.1H3"></path>
-                    <path d="M15.1 18H3"></path>
-                </svg>
-            </span>
-            <input class="metadata-property-key-input" type="text" value="date">
+    <div class="metadata-property" tabindex="0" :data-property-key="propertyKey" data-property-type="date">
+      <div class="metadata-property-key">
+        <span class="metadata-property-icon" aria-disabled="false">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-text">
+            <path d="M17 6.1H3"></path>
+            <path d="M21 12.1H3"></path>
+            <path d="M15.1 18H3"></path>
+          </svg>
+        </span>
+        <input class="metadata-property-key-input" type="text" :value="propertyKey" placeholder="Key">
+      </div>
+      <div class="metadata-property-value">
+        <div class="metadata-input-longtext mod-truncate" placeholder="Value" contenteditable="true" tabindex="0">
+          {{ propertyValue }}
         </div>
-        <div class="metadata-property-value">
-            <div class="metadata-input-longtext mod-truncate" placeholder="Empty" contenteditable="true" tabindex="0">
-                <p>2023-10-26</p>
-            </div>
-            <div class="metadata-link" style="display: none;">
-                <div class="metadata-link-inner">
-                    <p>2023-10-26</p>
-                </div>
-                <div class="metadata-link-flair">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-pencil">
-                        <line x1="18" y1="2" x2="22" y2="6"></line>
-                        <path d="M7.5 20.5 19 9l-4-4L3.5 16.5 2 22z"></path>
-                    </svg>
-                </div>
-            </div>
-        </div>
-        <div class="clickable-icon metadata-property-warning-icon" aria-label="Type mismatch, expected Text" style="display: none;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-alert-triangle">
-                <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
-                <line x1="12" y1="9" x2="12" y2="13"></line>
-                <line x1="12" y1="17" x2="12.01" y2="17"></line>
-            </svg>
-        </div>
+      </div>
+      <div class="clickable-icon metadata-property-warning-icon" aria-label="Type mismatch, expected Text" style="display: none;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-alert-triangle">
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"></path>
+          <line x1="12" y1="9" x2="12" y2="13"></line>
+          <line x1="12" y1="17" x2="12.01" y2="17"></line>
+        </svg>
+      </div>
     </div>
-</template>
-
-<script setup>
-</script>
-
-<style scoped>
-@import "../assets/obsidian.css";
-</style>
+  </template>
+  
+  <script setup>
+  
+  // Define props
+  const props = defineProps(['propertyKey', 'propertyValue']);
+  const { propertyKey, propertyValue } = props;
+  </script>
+  
+  <style scoped>
+    @import "../assets/obsidian.css";
+  </style>
+  
